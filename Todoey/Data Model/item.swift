2f81b6feb0dc6c36c33aed32eb8e2e52c4a307a1
Andreas Pafitis <agpafitis@gmail.com>
2018-04-24 17:35:59 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Andreas Pafitis on 23/04/2018.
+//  Copyright © 2018 Andreas Pafitis. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
